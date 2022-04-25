fb52372049d2d696d19d9918b813735ca53988dc
RolandYambao <rolandyambao@gmail.com>
2022-04-24 22:24:32 -0700

    Updated

---
+++ src/components/Test.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Test</p>
+            </div>
+        );
+
+    }
+}
+
+export default Test;
